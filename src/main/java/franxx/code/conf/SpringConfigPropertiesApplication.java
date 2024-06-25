fdde8c845ee23f3f39f81d0805bfcfcaa2a7048a
Hilmi AM <imiia75775@proton.me>
2024-06-25 15:15:36 +0700

    Externalized-Properties-File

--- src/main/java/franxx/code/conf/SpringConfigPropertiesApplication.java
+++ src/main/java/franxx/code/conf/SpringConfigPropertiesApplication.java
@@ -1,13 +1,29 @@
 package franxx.code.conf;
 
+import franxx.code.conf.converter.StringToDateConv;
+import franxx.code.conf.properties.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.convert.ApplicationConversionService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.ConversionService;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        AppProperties.class
+})
 public class SpringConfigPropertiesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringConfigPropertiesApplication.class, args);
-	}
+    @Bean
+    public ConversionService conversionService(StringToDateConv toDateConv) {
+        ApplicationConversionService service = new ApplicationConversionService();
+        service.addConverter(toDateConv);
+        return service;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigPropertiesApplication.class, args);
+    }
 
 }
---
+++ src/main/java/franxx/code/conf/runner/AppRunnerProp.java
@@ -0,0 +1,23 @@
+package franxx.code.conf.runner;
+
+import franxx.code.conf.properties.AppProperties;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class AppRunnerProp implements ApplicationRunner {
+
+    private AppProperties properties;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info(properties.getName());
+        log.info(String.valueOf(properties.getVersion()));
+        log.info(String.valueOf(properties.isProductionMode()));
+    }
+}
